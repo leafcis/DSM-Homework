3480560e833db3d79cdab6f050da72accb981963
unknown <leaf_cis@naver.com>
2020-03-03 10:38:28 +0900

    구조화 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import Login from './Login'
+// import Login from './Login'
 
 function App() {
   return (
     <div className="App">
-<Login />
+
     </div>
   );
 }
