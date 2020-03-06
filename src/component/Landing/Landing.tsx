a935902a511dc5bb2c897dd1c0003a85fd85dd1a
unknown <leaf_cis@naver.com>
2020-03-06 17:02:11 +0900

    작업

--- src/component/Landing/Landing.tsx
+++ src/component/Landing/Landing.tsx
@@ -8,7 +8,16 @@ interface LandingProps {
  const Landing: React.FC<LandingProps> = ({}) => {
   return (
     <S.LandingWrapper>
-      
+      <S.LandingDescription>
+        <div className="description">
+          <div>문제를 손쉽게 만들어 보세요</div>
+          <div>문제를 공유하고 같이 풀어보세요</div>
+          <div>손쉽게 채점해 보세요</div>
+        </div>
+        <S.Button>
+          시작하기
+        </S.Button>
+      </S.LandingDescription>
     </S.LandingWrapper>
   )
  }
--- src/utils/style/GlobalStyle.tsx
+++ src/utils/style/GlobalStyle.tsx
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
+    font-family: 'NanumSquare', sans-serif;
     margin: 0;
   }
 `
