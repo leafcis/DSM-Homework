95dcf82ddb8fe2b35a01db2e0c47a1b022d56ed4
unknown <leaf_cis@naver.com>
2020-03-08 17:02:11 +0900

    디자인 변경

--- src/component/Landing/LandingStyle.tsx
+++ src/component/Landing/LandingStyle.tsx
@@ -5,35 +5,46 @@ export const LandingWrapper = styled.div`
   min-height: 100vh;
   background: rgb(2,0,36);
   background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 60%, rgba(0,212,255,1) 100%);
-  box-sizing: border-box;
   display: flex;
   justify-content: center;
-  padding: 100px 0px;
+
+  & > div{
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    width: 60vw;
+    background-color: #ffffff;
+    justify-content: center;
+    padding: 200px 0px;
+    align-items: center;
+  }
 `
 
 export const LandingDescription = styled.div`
   display: flex;
-  height: 700px;
+  height: 425px;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  color: #ffffff; 
+  color: #000000; 
   font-weight: 1000;
-  font-size: 50px;
+  font-size: 40px;
 
   & .description {
     text-align: center;
-    line-height: 100px;
+    line-height: 60px;
   }
 `
 
 export const Button = styled.button`
   cursor: pointer;
-  height: 100px;
-  width: 270px;
-  font-size: 50px;
-  border-radius: 100px;
+  border: none;
+  height: 90px;
+  width: 220px;
+  font-size: 27.5px;
   font-weight: 1000;
+  background: #5e18af;
+  color: #ffffff;
 
   &:focus {
     outline: none;
