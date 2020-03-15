8d1583180ac04e1bc9bb4a5f2d5930e3e8157f1a
unknown <leaf_cis@naver.com>
2020-03-15 11:09:23 +0900

    모달 구조 정형화

---
+++ src/utils/Modal/ModalContainer/Modal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface ModalContainerProps {
+
+}
+
+const ModalContainer: React.FC<ModalContainerProps> = () => {
+  return (
+    <></>
+  )
+}
+
+export default ModalContainer;
