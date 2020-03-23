19ec7ff9ca6ac618eb00ab5061eb3f11bb8afc41
unknown <leaf_cis@naver.com>
2020-03-23 19:18:11 +0900

    리듀서 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
